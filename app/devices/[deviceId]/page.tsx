66e61b406771cccfa2038bc5144abafa292dd543
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-07-26 21:33:03 +0300

    feat: add GC-09 dynamic device detail route

---
+++ app/devices/[deviceId]/page.tsx
@@ -0,0 +1,10 @@
+import Gc2DeviceDetail from "@/components/devices/gc2-device-detail";
+
+export default async function DeviceDetailPage({
+  params,
+}: {
+  params: Promise<{ deviceId: string }>;
+}) {
+  const { deviceId } = await params;
+  return <Gc2DeviceDetail deviceId={decodeURIComponent(deviceId)} />;
+}
