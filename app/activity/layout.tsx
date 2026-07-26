e1ec68d633a5a7733ea6494d0366b65b3979742b
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-07-26 23:17:59 +0300

    refactor: remove duplicate activity timeline overlay

--- app/activity/layout.tsx
+++ app/activity/layout.tsx
@@ -1,12 +1,5 @@
 import type { ReactNode } from "react";
 
-import OperationsTimelineDeck from "@/components/activity/operations-timeline-deck";
-
 export default function ActivityLayout({ children }: { children: ReactNode }) {
-  return (
-    <>
-      {children}
-      <OperationsTimelineDeck />
-    </>
-  );
+  return children;
 }
