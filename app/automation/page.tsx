22609ba82782d5a6e4aa81798b9e62cf99006965
Ahmet Kerem Kuku <ahmet10kuku@gmail.com>
2026-07-28 17:06:47 +0300

    feat: mount automation rule editor overlay

--- app/automation/page.tsx
+++ app/automation/page.tsx
@@ -1,5 +1,11 @@
 import Gc2AutomationPolicy from "@/components/automation/gc2-automation-policy";
+import Gc2AutomationRuleEditorLauncher from "@/components/automation/gc2-automation-rule-editor-launcher";
 
 export default function AutomationPage() {
-  return <Gc2AutomationPolicy />;
+  return (
+    <>
+      <Gc2AutomationPolicy />
+      <Gc2AutomationRuleEditorLauncher />
+    </>
+  );
 }
